27dcd3aa74f6345ee85eddf47b5ef7121c4646c2
leejiwon6315 <leejiwon6315@naver.com>
2021-02-20 15:16:55 +0900

    fetch data

--- src/Components/NewsList.tsx
+++ src/Components/NewsList.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
 
@@ -18,17 +20,37 @@ const NewsListBlock = styled.div`
 interface Props {}
 
 const NewsList: React.FC<Props> = () => {
-  const sampleArticle = {
-    title: "title",
-    description: "description",
-    url: "https://naver.com",
-    urlToImage: "https://via.placeholder.com/160",
-  };
+  const [articles, setArticles] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetch = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(
+          "http://newsapi.org/v2/top-headlines?country=kr&apiKey=a47e22c8cee946bcb196a32f3f795e5e"
+        );
+        setArticles(res.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetch();
+  }, []);
+
+  if (loading) {
+    return <NewsListBlock>Loading...</NewsListBlock>;
+  }
+  if (!articles) {
+    return null;
+  }
+
   return (
     <NewsListBlock>
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
+      {articles.map((elem: any) => (
+        <NewsItem article={elem} key={elem.url} />
+      ))}
     </NewsListBlock>
   );
 };
